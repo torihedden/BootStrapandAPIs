a39ac68cd68c9a4b87a9460eda45156f744e7caa
Tori Hedden <tori.hedden@gmail.com>
2016-03-02 15:53:40 -0500

    adds some style thanks to bootstrap

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,16 +1,17 @@
 $(function(){
    'use strict';
 
-   var locationInput = $("input").val();
-   console.log(locationInput);
+   var locationVal = $(".locationInput").val();
+   console.log(locationVal);
    //for some reason it doesn't seem to like the locationInput variable
-   //
+
   //  $.getJSON("https://api.yelp.com/v2/search/?location=" + locationInput + "&sort=1&limit=3&category_filter=icecream", function(json) {
   //    console.log("locationInput");
   //  });
 
-//append the results of the query to three divs
-//index 0, then index 1, then index 2
+
+
+//append the first result of the query to div
 //will they be removed from the page if you hit refresh?
 //or give user an option to clear the old search and
 
